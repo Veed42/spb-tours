e9e8cb477bde173f536ff16f59cc35e43b05d256
Veed42 <valeraabramov96@gmail.com>
2024-05-17 21:23:29 +0300

    Update up home and admin Изменил роуты гидов админка хоум пейдж

--- routes/web.php
+++ routes/web.php
@@ -80,6 +80,11 @@ Route::group([
 });
 
 //User route
+Auth::routes();
+Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
+Route::post('/login', [LoginController::class, 'index'])->name('login');
+
+
 
 Route::group(['namespace'=>'User'], function (){
     Route::get('/', [HomeController::class, 'index'])->name('/');
@@ -90,9 +95,6 @@ Route::group(['namespace'=>'User'], function (){
 });
 
 
-Auth::routes();
-Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
-Route::post('/login', [LoginController::class, 'index'])->name('login');
 
 
 // Clear application cache:
